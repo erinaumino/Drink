058ad7a08db71c0c25a77dee46959f4c900a8ed8
海野恵凜那 <erina@unnomegumirinna-no-macbook-pro.local>
2017-01-31 02:55:15 +0900

    Initial Commit

---
+++ Drink/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Drink
+//
+//  Created by 海野恵凜那 on 2017/01/31.
+//  Copyright © 2017年 erina.umino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
